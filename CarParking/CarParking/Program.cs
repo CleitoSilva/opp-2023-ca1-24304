33c575244782d0b4d51073cb3bee61747bf7250b
CleitoSilva <129744824+CleitoSilva@users.noreply.github.com>
2023-11-27 11:29:01 +0000

    Add files via upload

---
+++ CarParking/CarParking/Program.cs
@@ -0,0 +1,55 @@
+﻿
+class Program
+{
+	static void Main()
+	{
+		List<User> garage1Users = new List<User>();
+		List<User> garage2Users = new List<User>();
+		List<User> garage3Users = new List<User>();
+
+		Random random = new Random();
+
+		for (int i = 0; i < 10; i++)
+		{
+			Garage1User user = new Garage1User();
+			user.HoursParked = random.Next(1, 10); 
+			garage1Users.Add(user);
+		}
+
+		for (int i = 0; i < 6; i++)
+		{
+			Garage2User user = new Garage2User();
+			user.HoursParked = random.Next(1, 10)
+			garage2Users.Add(user);
+		}
+
+		for (int i = 0; i < 8; i++)
+		{
+			Garage3User user = new Garage3User();
+			user.HoursParked = random.Next(1, 10); 
+			garage3Users.Add(user);
+		}
+
+		// Display charges for each garage
+		DisplayCharges(garage1Users, "Garage 1");
+		DisplayCharges(garage2Users, "Garage 2");
+		DisplayCharges(garage3Users, "Garage 3");
+	}
+
+	// Method to display charges for a garage
+	static void DisplayCharges(List<User> users, string garageName)
+	{
+		double totalCharge = 0;
+
+		Console.WriteLine($"Charges for {garageName}:");
+		foreach (var user in users)
+		{
+			double charge = user.CalculateCharges();
+			totalCharge += charge;
+
+			Console.WriteLine($"Registration Number: {user.RegistrationNumber}, Hours Parked: {user.HoursParked}, Charge: €{charge:F2}");
+		}
+
+		Console.WriteLine($"Total Charges for {garageName}: €{totalCharge:F2}\n");
+	}
+}
---
+++ CarParking/CarParking/abstract/Carparking.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using Faker;
+
+// Abstract class for User
+public abstract class User
+{
+	public string RegistrationNumber { get; set; } = string.Empty;
+	public int HoursParked { get; set; }
+
+	public abstract double CalculateCharges();
+}
+public class Garage1User : User
+{
+	public Garage1User()
+	{
+		var faker = new Faker();
+		RegistrationNumber = faker.Vehicle.Vin();
+
+	}
+	public override double CalculateCharges()
+	{
+		double minimumFee = 2.00;
+		double additionalFee = 0.50 * Math.Max(0, HoursParked - 3);
+		return Math.Min(minimumFee + additionalFee, 10.00);
+	}
+}
+public class Garage2User : User
+{
+	public Garage2User()
+	{
+		var faker = new Faker();
+		RegistrationNumber = faker.Vehicle.Vin();
+	}
+
+	public override double CalculateCharges()
+	{
+		double minimumFee = 2.00;
+		double additionalFee = 0.60 * Math.Max(0, HoursParked - 3);
+		return Math.Min(minimumFee + additionalFee, 10.00);
+	}
+}
+
+public class Garage3User : User
+{
+	public Garage3User()
+	{
+		var faker = new Faker();
+		RegistrationNumber = faker.Vehicle.Vin();
+	}
+
+	public override double CalculateCharges()
+	{
+		double minimumFee = 2.00;
+		double additionalFee = 0.75 * Math.Max(0, HoursParked - 3);
+		return Math.Min(minimumFee + additionalFee, 10.00);
+	}
+}
